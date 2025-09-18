d1b31338a608cef67d2eaca11a06acca2fd9226a
aedotcom <onew.choi@gmail.com>
2025-09-18 16:44:40 +0900

    파일추가

---
+++ src/pages/api/bullinfo/list.js
@@ -0,0 +1,7 @@
+import {bulls} from 'data/bullsinfo';
+import cors from 'utils/cors';
+
+export default async function handler(req, res) {
+  await cors(req, res);
+  return res.status(200).send({ bulls });
+}
